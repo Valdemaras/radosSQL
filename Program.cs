4e6a44e533682c20c8a1c73d721eb8f65b7147ad
Valdemaras Jasaitis <valdemaras.jasaitis@jotron.com>
2022-06-21 16:09:18 +0300

    Test implementation

--- Program.cs
+++ Program.cs
@@ -1,27 +1,36 @@
 ﻿using Dapper;
 using Microsoft.Data.Sqlite;
 
-var connection = new SqliteConnection("Data Source=file:///test_metadata:hellodc1/main.db?vfs=ceph");
+void loadCephSqlite()
+{
+    var test = new SqliteConnection();
+    test.Open();
+    test.LoadExtension("libcephsqlite.so");
+}
+
+loadCephSqlite();
+var connection = new SqliteConnection("Data Source=file:///test_metadata:hellodc1/metadata.db?vfs=ceph");
 using (connection)
 {
-    connection.LoadExtension("libcephsqlite.so");
+
     connection.Open();
     using (var trans = connection.BeginTransaction())
     {
         try
         {
-            connection.Execute("DROP TABLE MdMessage");
-            connection.Execute("DROP TABLE MdMessageDb");
-            connection.Execute("CREATE TABLE MdMessage (UniqueId TEXT, Class TINYINT, DID TINYINT, SID TINYINT, Timestamp REAL, Data TEXT)");
+            connection.Execute("CREATE TABLE MdMessage (UniqueId TEXT, Class TINYINT, DID INT, SID INT, Timestamp REAL, Data TEXT)");
             connection.Execute("CREATE TABLE MdMessageDb (UniqueId TEXT, Key TEXT, Value TEXT)");
             connection.Execute("CREATE INDEX idx_metadatadb ON MdMessage (DID, SID, Timestamp)");
             connection.Execute("CREATE INDEX idx_metadatatimestampdb ON MdMessage (Timestamp)");
             trans.Commit();
         }
-        catch (Exception)
+        catch (Exception ex)
         {
+            Console.WriteLine(ex.ToString());
             trans.Rollback();
         }
     }
+    
+    Console.WriteLine("Tables created successfully");
 }
 
